aed17ebbea6ca21855b649efd2471558309063a9
juanevangelista1 <juan.evangelista.nascimentoo@gmail.com>
2022-11-16 19:00:11 -0300

    modified:   index.html modified:   main.js modified:   style.css

--- main.js
+++ main.js
@@ -1,67 +1,48 @@
-// Abrir e fechar Menu
-const nav = document.querySelector('#header nav')
-const toggle = document.querySelectorAll('nav .toggle')
+// // Abrir e fechar Menu
+// const nav = document.querySelector('#header nav')
+// const toggle = document.querySelectorAll('nav .toggle')
 
-const buttonShow = document.querySelectorAll(
-  '#buttonBusiness01, #buttonBusiness02, #buttonBusiness03'
-)
+// const buttonShow = document.querySelectorAll(
+//   '#buttonBusiness01, #buttonBusiness02, #buttonBusiness03'
+// )
 
-for (element of buttonShow) {
-  element.addEventListener('click', function () {
-    console.log('funcionou')
-  })
-}
+// for (element of buttonShow) {
+//   element.addEventListener('click', function () {
+//     console.log('funcionou')
+//   })
+// }
 
+// for (var element of toggle) {
+//   element.addEventListener('click', function () {
+//     nav.classList.toggle('show')
+//     evento
+//   })
+// }
 
+// const links = document.querySelectorAll('nav ul li a')
 
+// for (var link of links) {
+//   link.addEventListener('click', function () {
+//     nav.classList.remove('show')
+//   })
+// }
 
+// // Shadow after scroll on header
 
+// const header = document.querySelector('#header')
+// const navHeight = header.offsetHeight
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-for (var element of toggle) {
-  element.addEventListener('click', function () {
-    nav.classList.toggle('show')
-    evento
-  })
-}
-
-const links = document.querySelectorAll('nav ul li a')
-
-for (var link of links) {
-  link.addEventListener('click', function () {
-    nav.classList.remove('show')
-  })
-}
-
-// Shadow after scroll on header
-
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
-
-window.addEventListener('scroll', function () {
-  if (window.scrollY >= navHeight) {
-    header.classList.add('scroll')
-  } else {
-    header.classList.remove('scroll')
-  }
-})
-window.addEventListener('scroll', function () {
-  if (window.scrollY >= navHeight) {
-    header.classList.add('scroll')
-  } else {
-    header.classList.remove('scroll')
-  }
-})
+// window.addEventListener('scroll', function () {
+//   if (window.scrollY >= navHeight) {
+//     header.classList.add('scroll')
+//   } else {
+//     header.classList.remove('scroll')
+//   }
+// })
+// window.addEventListener('scroll', function () {
+//   if (window.scrollY >= navHeight) {
+//     header.classList.add('scroll')
+//   } else {
+//     header.classList.remove('scroll')
+//   }
+// })
