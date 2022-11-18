58d0c68f6e11dcf221882005f3d18a2cea909f24
juanevangelista1 <juan.evangelista.nascimentoo@gmail.com>
2022-11-18 10:40:37 -0300

    Add Jquery

--- main.js
+++ main.js
@@ -2,28 +2,34 @@
 
 $('#toggleButton1').click(function () {
   $('#textShow1').toggle().css('#textShow1', 'hidden')
-  if ($('#textShow1').is(':visible')) {
-    $('#toggleButton1').attr('id', 'arrowUpButton1')
+  if ($('#textShow1').is(':visible') && $('#footerCard1').is(':visible')) {
+    $('#toggleButton1').attr('id', 'arrowUpButton1') &&
+      $('#footerCard1').attr('id', 'footerCardHide1')
   } else {
-    $('#arrowUpButton1').attr('id', 'toggleButton1')
+    $('#arrowUpButton1').attr('id', 'toggleButton1') &&
+      $('#footerCardHide1').attr('id', 'footerCard1')
   }
 })
 
 $('#toggleButton2').click(function () {
   $('#textShow2').toggle()
-  if ($('#textShow2').is(':visible')) {
-    $('#toggleButton2').attr('id', 'arrowUpButton2')
+  if ($('#textShow2').is(':visible') && $('#footerCard2').is(':visible')) {
+    $('#toggleButton2').attr('id', 'arrowUpButton2') &&
+      $('#footerCard2').attr('id', 'footerCardHide2')
   } else {
-    $('#arrowUpButton2').attr('id', 'toggleButton2')
+    $('#arrowUpButton2').attr('id', 'toggleButton2') &&
+      $('#footerCardHide2').attr('id', 'footerCard2')
   }
 })
 
 $('#toggleButton3').click(function () {
   $('#textShow3').toggle()
-  if ($('#textShow3').is(':visible')) {
-    $('#toggleButton3').attr('id', 'arrowUpButton3')
+  if ($('#textShow3').is(':visible') && $('#footerCard3').is(':visible')) {
+    $('#toggleButton3').attr('id', 'arrowUpButton3') &&
+      $('#footerCard3').attr('id', 'footerCardHide3')
   } else {
-    $('#arrowUpButton3').attr('id', 'toggleButton3')
+    $('#arrowUpButton3').attr('id', 'toggleButton3') &&
+      $('#footerCardHide3').attr('id', 'footerCard3')
   }
 })
 
